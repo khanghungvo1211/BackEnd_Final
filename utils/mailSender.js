e4338072fb84c4b18757916723741fa96ca8e30c
khanghungvo1211 <hungkhang02@gmail.com>
2023-12-13 22:45:55 +0700

    13_12_2023

---
+++ utils/mailSender.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, title, body) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                service: "gmail",
+                host: "smtp.gmail.com",
+                port: 465,
+                secure:true,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: 'StudyNotion || CodeHelp - by Babbar',
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+
+
+module.exports = mailSender;
